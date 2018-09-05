e54920be62f1151e8ea88e5fc3c4c0f01230267f
nick <nicklgw@aliyun.com>
2018-09-05 21:50:01 +0800

    在base-files.mk中区分板型,并加载不同的配置/etc/inittab

--- target/linux/ar71xx/base-files.mk
+++ target/linux/ar71xx/base-files.mk
@@ -1,3 +1,30 @@
 define Package/base-files/install-target
 	rm -f $(1)/etc/config/network
+
+	for inittab in $(1)/etc/inittab*; do \
+		if [ -f "$$$$inittab" ]; then \
+			rm -f "$$$$inittab"; \
+		fi \
+	done
+
+ifeq ($(CONFIG_TARGET_ar71xx_generic_LDS_G104),y)
+
+	if [ -f $(PLATFORM_DIR)/base-files/etc/inittab_lds_g104 ]; then \
+		$(CP) $(PLATFORM_DIR)/base-files/etc/inittab_lds_g104 $(1)/etc/inittab; \
+	fi
+
+endif
+
+ifeq ($(CONFIG_TARGET_ar71xx_generic_LDS_G151),y)
+
+	if [ -f $(PLATFORM_DIR)/base-files/etc/inittab_lds_g151 ]; then \
+		$(CP) $(PLATFORM_DIR)/base-files/etc/inittab_lds_g151 $(1)/etc/inittab; \
+	fi
+
+endif
+
+	if [ ! -e $(1)/etc/inittab ]; then \
+		$(CP) $(PLATFORM_DIR)/base-files/etc/inittab $(1)/etc/inittab; \
+	fi
+
 endef
