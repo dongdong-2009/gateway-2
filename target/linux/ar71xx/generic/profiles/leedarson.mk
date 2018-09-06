3ee80192b431c470ed859d8edf1a42a0b7d301d8
nick <nicklgw@aliyun.com>
2018-09-06 18:48:21 +0800

    为lds-g151板型添加Kernel内核支持

--- target/linux/ar71xx/generic/profiles/leedarson.mk
+++ target/linux/ar71xx/generic/profiles/leedarson.mk
@@ -23,7 +23,21 @@ endef
 
 define Profile/LDS_G104/Description
 	Package set optimized for the Leedarson G104 device.
-	It's Leedarson Education Gateway.And its model is lds.gateway.g104
+	It's Leedarson Education Gateway And its model is lds.gateway.g104
+endef
+
+define Profile/LDS_G151
+	NAME:=Leedarson LDS-G151 board
+	PACKAGES:=$(IOE_BASE) $(TEST_TOOLS) $(WIFI_OPEN) \
+			kmod-usb-serial kmod-usb-serial-pl2303 kmod-ath10k \
+			qca-legacy-uboot-lds-g151
+endef
+
+define Profile/LDS_G151/Description
+	Package set optimized for the Leedarson G151 device.
+	It's called A023 project for The Home Depot, Inc. 
+	And its model is lds.gateway.g151
 endef
 
 $(eval $(call Profile,LDS_G104))
+$(eval $(call Profile,LDS_G151))
