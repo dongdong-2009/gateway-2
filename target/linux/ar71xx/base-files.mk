626a55bdf7c891a6b692ea431fae20421cc72837
nicklgw <nicklgw@aliyun.com>
2018-10-08 19:48:04 +0800

    添加在linux shell中烧写证书工具cert

--- target/linux/ar71xx/base-files.mk
+++ target/linux/ar71xx/base-files.mk
@@ -1,3 +1,10 @@
+define Build/Compile
+	$(call Build/Compile/Default)
+	$(TARGET_CC) -o $(PKG_BUILD_DIR)/cert $(PLATFORM_DIR)/src/cert.c
+endef
+
 define Package/base-files/install-target
-	rm -f $(1)/etc/config/network	
+	mkdir -p $(1)/sbin
+	$(CP) $(PKG_BUILD_DIR)/cert $(1)/sbin
+	rm -f $(1)/etc/config/network
 endef
